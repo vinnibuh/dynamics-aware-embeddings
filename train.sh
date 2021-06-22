48260528e906549895da274cc149f44bc388efff
vinnibuh <ivchyaroslav@gmail.com>
2021-06-22 12:50:28 +0000

    run decoders for different kl

--- train.sh
+++ train.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-python3 scripts/visualise_clusters.py --env dmc_hopper_hop --episodes_dir ./datasets/dreamer/dmc_hopper_hop/dreamer_finetuned --n_samples 100 --encoder_name DynE-4-kl-1e-3 --traj_len 4 --wandb True
-python3 scripts/visualise_clusters.py --env dmc_hopper_hop --episodes_dir ./datasets/dreamer/dmc_hopper_hop/dreamer_finetuned --n_samples 100 --encoder_name DynE-4-kl-1e-2 --traj_len 4 --wandb True
-python3 scripts/visualise_clusters.py --env dmc_hopper_hop --episodes_dir ./datasets/dreamer/dmc_hopper_hop/dreamer_finetuned --n_samples 100 --encoder_name DynE-4-kl-1e-1 --traj_len 4 --wandb True
-python3 scripts/visualise_clusters.py --env dmc_hopper_hop --episodes_dir ./datasets/dreamer/dmc_hopper_hop/dreamer_finetuned --n_samples 100 --encoder_name DynE-4-kl-1 --traj_len 4 --wandb True
-
-dvc add logdir
-dvc add models
-dvc push
-git add .
-git commit -m 'fixed loss, added plots'
-git push vinnibuh loss_modification
+python3 scripts/train_decoder.py --name DynE-4-kl-1e-3 --env dmc_hopper_hop --wandb True --traj_len 4 
+python3 scripts/train_decoder.py --name DynE-4-kl-1e-2 --env dmc_hopper_hop --wandb True --traj_len 4 
+python3 scripts/train_decoder.py --name DynE-4-kl-1e-1 --env dmc_hopper_hop --wandb True --traj_len 4 
+python3 scripts/train_decoder.py --name DynE-4-kl-1 --env dmc_hopper_hop --wandb True --traj_len 4 
